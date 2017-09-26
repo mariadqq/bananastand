019e583dbdd33454ba643a9595cfd429498590e3
Maria Dahlquist <maria.dahlquist@svenskaspel.se>
2017-09-26 14:35:22 +0200

    Added calculation of banansplit using variables

--- store.js
+++ store.js
@@ -1,8 +1,15 @@
-
+const bananasplit=20
+const spoon=5
 
 function greet (){
 
 	console.log('Välkommen!');
-}
+};
+function counter(b, s){
 
+	console.log('Counter');
+	//console.log(20*5+5*3);
+	console.log(bananasplit*b+spoon*s);
+};
 greet();
+counter(5, 3);
