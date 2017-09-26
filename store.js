9eca6a29dfd9914f530f47797f9cadfc3e87b2ec
Maria Dahlquist <maria.dahlquist@svenskaspel.se>
2017-09-26 15:00:18 +0200

    Added prices, loop and objects

--- store.js
+++ store.js
@@ -1,5 +1,21 @@
-const bananasplit=20
-const spoon=5
+
+
+var menu = [{name:'bananasplit', price:20}, 
+			{name:'spoon', 	price:4},
+			{name:'topping', price:5}];
+
+function describeItem(item) {
+	console.log('Item: '+ item.name + ' Price: '+ item.price);
+
+}
+function describeMenu() {
+	console.log('Describe menu');
+	
+	for (i =0; i < menu.length; i++) {
+		describeItem(menu[i]);
+	}
+
+}
 
 function greet (){
 
@@ -9,7 +25,8 @@ function counter(b, s){
 
 	console.log('Counter');
 	//console.log(20*5+5*3);
-	console.log(bananasplit*b+spoon*s);
+	//console.log(bananasplit*b+spoon*s);
 };
 greet();
+describeMenu();
 counter(5, 3);
